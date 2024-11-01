c32ed64040580f81147b69a5d19ef8164b4aaf96
adavidyuk <ganna.d@webgroup-limited.com>
2024-11-02 00:54:21 +0100

    Update Angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    EducationComponent,
+    ExperienceComponent,
+
+    ProjectsComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    HomeComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
